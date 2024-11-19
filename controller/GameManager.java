f69fca34dbc41ed69f0bcb26988257b7ad0b3f20
Abrorjon Asralov <asralov@arizona.edu>
2024-11-19 09:05:59 -0700

    added GameManager.java

---
+++ controller/GameManager.java
@@ -0,0 +1,22 @@
+package controller;
+
+import model.GameBoard;
+
+public class GameManager 
+{
+    private GameBoard board;
+    private int move_count;
+
+    public GameManager()
+    {
+        this.board = new GameBoard();
+        this.move_count = 0;
+    }
+
+    
+
+
+    // private void reset_move_count() {this.move_count = 0;}
+
+
+}
