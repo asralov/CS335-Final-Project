eef509c02632ff2633aade5eeb79930cc7dd3de2
pulyau <83565561+pulyau@users.noreply.github.com>
2024-11-26 08:55:14 -0700

    Added intial tests

--- tests/GameBoardTest.java
+++ tests/GameBoardTest.java
@@ -1,5 +1,22 @@
 package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.GameBoard;
+
 // Pulat's task
 public class GameBoardTest {
     
+	private GameBoard gameBoard = new GameBoard();
+	
+	@Test
+	public void TestGetCell() {
+		assertNull(gameBoard.get_cell(-1, 0));
+		assertNull(gameBoard.get_cell(8, 0));
+		assertNull(gameBoard.get_cell(-1, -1));
+		assertNull(gameBoard.get_cell(0, 8));
+	}
+
 }
--- tests/PieceTest.java
+++ tests/PieceTest.java
@@ -1,5 +1,36 @@
 package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import model.*;
 // Pulat's task
 public class PieceTest {
+	
+	private Piece newPiece = new Piece(Color.BLACK, 0, 1);
+	
+	@Test
+	public void TestPieceDefaultColorAndCoordinates() {
+		assertEquals(Color.BLACK, newPiece.getColor());
+		assertEquals(1, newPiece.getColumn());
+		assertEquals(0, newPiece.getRow());
+	}
+	
+	@Test
+	public void TestIsKing() {
+		assertFalse(newPiece.isKing());
+		newPiece.ToKing();
+		assertTrue(newPiece.isKing());
+	}
+
+//	@Test
+//	public void TestValidMove() {
+//		assertTrue()
+//	}
+	
+	@Test
+	public void ToString() {
+		System.out.println(newPiece);
+	}
     
 }
